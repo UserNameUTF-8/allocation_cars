ad06770f87b869e8120013fb038ce85bf5bfd830
usernameutf-8 <essid101010@gmail.com>
2023-11-28 09:44:01 +0100

    database _

--- database/MainDB.py
+++ database/MainDB.py
@@ -11,11 +11,15 @@ USERNAME = os.getenv('USERNAME_')
 PASSWORD = os.getenv('PASSWORD')
 HOSTNAME = os.getenv('HOSTNAME')
 DATABASE = os.getenv('DATABASE')
+PORT = os.getenv('PORT')
 
-mainEngine = create_engine(url=f'mysql://{USERNAME}:{PASSWORD}@{HOSTNAME}/{DATABASE}', echo=True)
+# mainEngine = create_engine(url=f'mysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}', echo=True)
+mainEngine = create_engine(
+    f"mysql://{USERNAME}:{PASSWORD}@mysql-1ab537cc-essid101010-3c12.a.aivencloud.com:21800/defaultdb")
 Session = sessionmaker(mainEngine)
 session = Session()
 Base = declarative_base()
 
 if __name__ == '__main__':
     Base.metadata.create_all(mainEngine)
+    print(mainEngine)
